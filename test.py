d918c0db33a702029296f5bb7166265fa1267566
Bianca Popa <56972854+BiancaP-hub@users.noreply.github.com>
2024-04-05 15:52:51 -0400

    Update test.py

--- test.py
+++ test.py
@@ -12,15 +12,15 @@ import argparse
 
 # Create a parser object and add arguments for alpha, beta, and gamma, modalities (list of strings, by default ['t2w', 't1w']) and batch size (default 1)
 test_parser = argparse.ArgumentParser(description='Test a model on a dataset')
-test_parser.add_argument('--alpha', type=float, default=0.5, help='Weight for the luminance component of the SSIM loss')
-test_parser.add_argument('--beta', type=float, default=0.5, help='Weight for the contrast component of the SSIM loss')
+test_parser.add_argument('--alpha', type=float, default=1, help='Weight for the luminance component of the SSIM loss')
+test_parser.add_argument('--beta', type=float, default=1, help='Weight for the contrast component of the SSIM loss')
 test_parser.add_argument('--gamma', type=float, default=1, help='Weight for the structure component of the SSIM loss')
 test_parser.add_argument('--modalities', nargs='+', default=['T2w', 'T1w'], help='List of modalities to use for training')
 test_parser.add_argument('--batch_size', type=int, default=1, help='Batch size for testing')
 # Add argument for the loss function (default 'ssim')
-test_parser.add_argument('--loss_function', type=str, default='combined', help='Loss function to use for testing')
+test_parser.add_argument('--loss_function', type=str, default='ssim', help='Loss function to use for testing')
 # Add argument for the multi-scale feature usage (default False)
-test_parser.add_argument('--use_multi_scale', action='store_false', help='Use multi-scale features in the model')
+test_parser.add_argument('--use_multi_scale', action='store_true', help='Use multi-scale features in the model')
 
 def test_model(modalities, model, test_dataset):
     """
@@ -43,7 +43,7 @@ def test_model(modalities, model, test_dataset):
     for images in test_dataset:
         image1, image2 = images[modalities[0]], images[modalities[1]]  
         test_output = model([image1, image2], training=False)  # Generate predictions
-        # save_fused_image(modalities, test_output, images['patient_id'])  # Save the fused image
+        save_fused_image(modalities, test_output, images['patient_id'], images['slice_number'])  # Save the fused image
         fmi_score = fmi(image1, image2, test_output, eng)  # Calculate FMI score between ground truth and prediction
         ssim_score = ssim([image1, image2], test_output)
         fmi_scores.append(fmi_score)
@@ -72,8 +72,6 @@ if __name__ == "__main__":
                                        custom_objects={'custom_loss': custom_loss})
             print("Loaded model with ssim loss and multi-scale features")
         else:
-            model = tf.keras.models.load_model(f"saved_models/fusion_model_alpha{args.alpha}_beta{args.beta}_gamma{args.gamma}_multi_ssim_loss.h5", 
-                                       custom_objects={'custom_loss': custom_loss})
             print("Loaded model with ssim loss")
     else:
         custom_loss = custom_combined_loss(alpha=args.alpha, beta=args.beta)
